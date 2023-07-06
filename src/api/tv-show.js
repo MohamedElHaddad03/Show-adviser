01350786a55a4f79dedd4a17648f1c6e0b1009be
MohamedElHaddad03 <130052591+MohamedElHaddad03@users.noreply.github.com>
2023-07-06 14:06:18 +0100

    .env

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,18 +1,17 @@
 import axios from "axios";
-import { BASE_URL, RECOMMENDATION_URL } from "../config";
-import { API_KEY_PARAM } from "../config";
+import { BASE_URL } from "../config";
 
 export class TVShowApi {
   static async fetchPopular() {
-    const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM} `);
+    const response = await axios.get(`${BASE_URL}tv/popular?api_key=${process.env.REACT_APP_API_KEY_PARAM} `);
     return response.data.results;
   }
   static async fetchRecommendation(tvShowID) {
-    const response = await axios.get(`${BASE_URL}tv/${tvShowID}/recommendations${API_KEY_PARAM} `);
+    const response = await axios.get(`${BASE_URL}tv/${tvShowID}/recommendations?api_key=${process.env.REACT_APP_API_KEY_PARAM} `);
     return response.data.results;
   }
   static async fetchByTitle(title) {
-    const response = await axios.get(`${BASE_URL}search/tv${API_KEY_PARAM}&query=${title} `);
+    const response = await axios.get(`${BASE_URL}search/tv?api_key=${process.env.REACT_APP_API_KEY_PARAM}&query=${title} `);
     return response.data.results;
   }
 }
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
  const BASE_URL = "https://api.themoviedb.org/3/";
- const API_KEY_PARAM = "?api_key=de58816af97336107cad8deb31ee0035";
+
  const BACKDROP_BASE_URL="https://image.tmdb.org/t/p/original/";
  const BACKDROP_MINI_URL="https://image.tmdb.org/t/p/w300/";
 
 
- export{BASE_URL,API_KEY_PARAM,BACKDROP_BASE_URL,BACKDROP_MINI_URL}
+ export{BASE_URL,BACKDROP_BASE_URL,BACKDROP_MINI_URL}
